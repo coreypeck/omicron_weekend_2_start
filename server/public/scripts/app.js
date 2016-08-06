51b24de8feaf5ad962fdef79be3fb9a7079db2c8
Corey Peck <coreypeck@gmail.com>
2016-08-06 18:25:22 -0500

    Added comments and removed unecessary variables

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
         type: "GET",
         url: "/data",
         success: function(data) {
+          //calls the blocks in and sets up the first person
             var $el = data.omicron;
             $middleman = $el;
             $(".person").fadeOut("fast", function() {
@@ -13,22 +14,23 @@ $(document).ready(function() {
             });
         }
     });
+    //sets event listeners and starts the timer
     $("#Next").on("click", goNext);
     $("#Prev").on("click", goPrev);
     timer = setTimeout(autoSwitch, 10000);
 });
+//initializes key variables
 var $middleman = [];
 var counter = 0;
 var timer;
-var whichBox = [];
-
+//function that replaces the person with the appropriate one based on which button was hit
 function replace() {
     $(".person").fadeOut("fast", function() {
         $(".person").replaceWith("<div class='person'>" + "Student Name: " + $middleman[counter].name + "<p class='personUsername'>Github Account: <a href='https://github.com/'" + $middleman[counter].git_username + ">https://github.com/" + $middleman[counter].git_username + "</a><p class='personShoutout'>Personal Message: " + $middleman[counter].shoutout + "</p></p></div>");
         $(".person").hide().fadeIn("fast");
     });
 }
-
+//function that moves to the next person
 function goNext() {
     counter++;
     resetAutoSwitch();
@@ -37,7 +39,7 @@ function goNext() {
     replace();
     $("#Next").focus();
 }
-
+//function that moves to the previous person
 function goPrev() {
     counter--;
     resetAutoSwitch();
@@ -46,7 +48,7 @@ function goPrev() {
     replace();
     $("#Prev").focus();
 }
-
+//function to check if the counter is at the point where it needs to get to ther other end of the index or not
 function checkCounter() {
     if (counter >= $middleman.length) {
         counter = 0;
@@ -54,7 +56,7 @@ function checkCounter() {
         counter = $middleman.length - 1;
     }
 }
-
+//the Function that activates when 10 seconds has passed with no buttons being pressed
 function autoSwitch() {
     counter++;
     movingForward();
@@ -66,19 +68,19 @@ function autoSwitch() {
     timer = setTimeout(autoSwitch, 10000);
     $("#Next").focus();
 }
-
+//resets the 10 second timer when next or prev are hit
 function resetAutoSwitch() {
     clearTimeout(timer);
     timer = setTimeout(autoSwitch, 10000);
 }
-
+//function that creates a box for each person and assigns them an ID based on the number they come in as
 function getBoxes() {
     for (var i = 0; i < $middleman.length; i++) {
         $("#boxHome").append("<div id='index" + i + "' class='square'></div>");
     }
     $("#index0").replaceWith("<div id='active' class='square'></div>");
 }
-
+//function to move the box index forward
 function movingForward() {
     if (counter > $middleman.length -1) {
         $('#active').replaceWith("<div id='index" + (counter-1) + "'class='square'></div>");
@@ -90,7 +92,7 @@ function movingForward() {
         $("#active").hide().fadeIn("fast");
     }
 }
-
+//function to move the box index backward
 function movingBackward() {
     if (counter < 0) {
         $('#active').replaceWith("<div id='index" + (counter + 1) + "'class='square'></div>");
