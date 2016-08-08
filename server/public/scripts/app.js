02dd5c971038afce40c978253fd3839910315384
Corey Peck <coreypeck@gmail.com>
2016-08-08 09:29:22 -0500

    fixed anchor tag concatenation error

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
         type: "GET",
         url: "/data",
         success: function(data) {
-          //calls the blocks in and sets up the first person
+            //calls the blocks in and sets up the first person
             var $el = data.omicron;
             $middleman = $el;
             $(".person").fadeOut("fast", function() {
                 getBoxes();
-                $(".person").replaceWith("<div class='person'>" + "Student Name: " + $middleman[counter].name + "<p class='personUsername'>Github Account: <a href='https://github.com/'" + $middleman[counter].git_username + ">https://github.com/" + $middleman[counter].git_username + "</a><p class='personShoutout'>Personal Message: " + $middleman[counter].shoutout + "</p></p></div>");
+                $(".person").replaceWith("<div class='person'>" + "Student Name: " + $middleman[counter].name + "<p class='personUsername'>Github Account: <a href='https://github.com/" + $middleman[counter].git_username + "'>'https://github.com/" + $middleman[counter].git_username + "'</a><p class='personShoutout'>Personal Message: " + $middleman[counter].shoutout + "</p></p></div>");
                 $(".person").hide().fadeIn("fast");
                 $("#Next").focus();
             });
@@ -26,7 +26,7 @@ var timer;
 //function that replaces the person with the appropriate one based on which button was hit
 function replace() {
     $(".person").fadeOut("fast", function() {
-        $(".person").replaceWith("<div class='person'>" + "Student Name: " + $middleman[counter].name + "<p class='personUsername'>Github Account: <a href='https://github.com/'" + $middleman[counter].git_username + ">https://github.com/" + $middleman[counter].git_username + "</a><p class='personShoutout'>Personal Message: " + $middleman[counter].shoutout + "</p></p></div>");
+        $(".person").replaceWith("<div class='person'>" + "Student Name: " + $middleman[counter].name + "<p class='personUsername'>Github Account: <a href='https://github.com/" + $middleman[counter].git_username + "'>'https://github.com/" + $middleman[counter].git_username + "'</a><p class='personShoutout'>Personal Message: " + $middleman[counter].shoutout + "</p></p></div>");
         $(".person").hide().fadeIn("fast");
     });
 }
@@ -62,7 +62,7 @@ function autoSwitch() {
     movingForward();
     checkCounter();
     $(".person").fadeOut("fast", function() {
-        $(".person").replaceWith("<div class='person'>" + "Student Name: " + $middleman[counter].name + "<p class='personUsername'>Github Account: <a href='https://github.com/'" + $middleman[counter].git_username + ">https://github.com/" + $middleman[counter].git_username + "</a><p class='personShoutout'>Personal Message: " + $middleman[counter].shoutout + "</p></p></div>");
+        $(".person").replaceWith("<div class='person'>" + "Student Name: " + $middleman[counter].name + "<p class='personUsername'>Github Account: <a href='https://github.com/" + $middleman[counter].git_username + "'>'https://github.com/'" + $middleman[counter].git_username + "'</a><p class='personShoutout'>Personal Message: " + $middleman[counter].shoutout + "</p></p></div>");
         $(".person").hide().fadeIn("fast");
     });
     timer = setTimeout(autoSwitch, 10000);
@@ -82,13 +82,13 @@ function getBoxes() {
 }
 //function to move the box index forward
 function movingForward() {
-    if (counter > $middleman.length -1) {
-        $('#active').replaceWith("<div id='index" + (counter-1) + "'class='square'></div>");
+    if (counter > $middleman.length - 1) {
+        $('#active').replaceWith("<div id='index" + (counter - 1) + "'class='square'></div>");
         $('.square').first().replaceWith("<div id='active' class='square'></div>");
         $("#active").hide().fadeIn("fast");
     } else {
         $('#active').next().replaceWith("<div id='active' class='square'></div>");
-        $('#active').replaceWith("<div id='index" + (counter-1) + "'class='square'></div>");
+        $('#active').replaceWith("<div id='index" + (counter - 1) + "'class='square'></div>");
         $("#active").hide().fadeIn("fast");
     }
 }
